6c0f67091d4e0e078d49c82c9eaf7485c76fd923
JoseAlexandreGiry <33418836+JoseAlexandreGiry@users.noreply.github.com>
2019-03-06 17:00:19 +0100

    Add files via upload
    Fichier de test java avec Fail, doit échouer

---
+++ calculatriceTest.java
@@ -0,0 +1,28 @@
+package fr.ensup.demo.tdd.test;
+
+import org.junit.Test;
+
+/**
+ * Created by jose on 06/03/2019.
+ */
+
+public class calculatriceTest {
+
+/*    private Calculatrice calculatrice;
+
+    public calculatriceTest() {
+    }
+
+    @Before
+    public void initialiserDonneestest() {
+        this.calculatrice = new Calculatrice();
+    }*/
+
+
+    @Test
+    public void testAddition() {
+        //fail("test fail");
+/*        int resultat = this.calculatrice.addition(3, 4);
+        Assert.assertEquals(7, (int)resultat);*/
+    }
+}
